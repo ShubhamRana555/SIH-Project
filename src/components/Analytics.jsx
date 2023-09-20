e010b1ca9afb6bb9ff87000b38ef9511d08cf0e3
Anshul <anshulraturi007@gmail.com>
2023-09-20 22:14:28 +0530

    Content updation

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -10,15 +10,20 @@ const Analytics = () => {
           <NeuronRenderer />
         </div>
         <div className='flex flex-col justify-center'>
-          <p className='text-[#00df9a] font-bold '>DATA ANALYTICS DASHBOARD</p>
+          <p className='text-[#00df9a] font-bold '>
+            Neuron : The basic building block of brain
+          </p>
           <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>
-            Manage Data Analytics Centrally
+            Mental Disorders through Neuron
           </h1>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
-            molestiae delectus culpa hic assumenda, voluptate reprehenderit
-            dolore autem cum ullam sed odit perspiciatis. Doloribus quos velit,
-            eveniet ex deserunt fuga?
+            Mental disorders are complex conditions that affect millions of
+            people worldwide. While their exact causes remain a subject of
+            ongoing research, scientists have made significant strides in
+            understanding the role of neurons in the development and
+            manifestation of these disorders. Neurons, the fundamental building
+            blocks of the nervous system, play a crucial role in our thoughts,
+            emotions, and behaviors.
           </p>
           <button className='bg-black text-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3'>
             Get Started
